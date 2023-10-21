74d4ad5bc81f6f4c6e0454a5feda0dc598c9c1aa
camimtzd <143425459+camimtzd@users.noreply.github.com>
2023-10-21 19:12:14 -0300

    validacion 21/10

--- Contactos/contactos.js
+++ Contactos/contactos.js
@@ -1,83 +1,43 @@
-/*var nombre = document.getElementById ('nombre');
-
-if(nombre.value === '' || nombre.value === null){
-    mensajesError.push('ingresá tu nombre');
-}*/
-
-function validarFormulario() 
+function validarFormulario()
 {   
+    var nombre = document.getElementById('nombre').value.trim();
+    var mensaje = document.getElementById('mensaje').value.trim();
+    var email = document.getElementById('email').value.trim();
+    var telefono = document.getElementById('telefono').value.trim();
 
-    valor = document.getElementById('nombre').value.trim();
-    
-if(valor == null || valor.length == 0) 
+    if(nombre == null || nombre.length == 0) 
 {
     
-    document.getElementById('msjnombre').innerHTML = "Ingresá tu nombre";
+    document.getElementById('msjnombre').innerHTML = "Ingrese su nombre";
     elemento = document.getElementById('nombre');
-    elemento.focus();
-    
+   
     return false;
-}       
+}   
 
-valor = document.getElementById('mensaje').value.trim();
 
-if (valor == null || valor.length == 0)
+else if(!(/^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail|edu)\.(?:|com|ar)+$/.test(email))){
+    document.getElementById('msjemail').innerHTML = "No es una dirección de email correcta";
+    elemento = document.getElementById('email');
+    
+    return false
+
+}
+
+else if (telefono > 10 || telefono == null){
+document.getElementById('msjtel').innerHTML = "No es un número de teléfono correcto";
+    elemento = document.getElementById('telefono');
+return false
+}
+
+else if(mensaje == null || mensaje.length == 0)
 {
 
-
-document.getElementById("msj").innerHTML = "Debes escribir un mensaje";
+document.getElementById('msj').innerHTML = "Debes escribir un mensaje";
 elemento = document.getElementById('mensaje');
-element.focus();
 
 return false;
 
 }
 return true
+
 }
-
-
-/* ESTE ES EL MAIL PERO NO ME LO TOMA, ME SALTA UN CARTEL AUTOMATICO
-
-
- valor = document.getElementById("email").value;
-        
-          
-        if (!(/^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail|edu)\.(?:|com|ar)+$/.test(valor)))
-        {
-            document.getElementById('msjmail').innerHTML = "No es una direccion de email correcta";
-            elemento = document.getElementById('email');
-            elemento.focus();
-
-            return false;
-        }
-
-return true
-}
-
-
-var formulario = document.getElementsByName('formulario')[0],
-                elementos = formulario.elements,
-                boton = document.getElementById('btn');
-            var validarNombre = function(e){
-                if (formulario.nombre.value == 0){
-                    alert("Completa el campo nombre");
-                    e.preventDefault();
-                  
-                }
-            };
-
-            var validarMensaje= function(e){
-                if (formulario.mensaje.value == 0);{
-
-                alert("Debes escribir un mensaje");
-                e.preventDefault();
-
-                }
-            };
-
-            var validar = function(e){
-                validarNombre(e);
-                validarMensaje(e);
-
-        }
-*/
